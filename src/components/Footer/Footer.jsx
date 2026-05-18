bdbca9a5da79ce59c2870d233fb61531c8dd7965
nhuyeumeo2811-del <nhuyeumeo2811@gmail.com>
2026-05-18 11:18:04 +0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import logoImage from '../../img/logo.png';
+import logoImg from '../../img/logo.png';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Footer = () => {
